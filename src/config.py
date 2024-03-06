be36f6cbad33632b88ede4ea31c71fce6dbbc002
emiliocramer <emilio.cramer101@gmail.com>
2024-03-06 13:17:34 -0500

    add config

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+VOSK_MODEL_PATH = '../model'
+RESPONSE_ASSISTANT_ID = os.environ.get('RESPONSE_ASSISTANT_ID')
+VERDICT_ASSISTANT_ID = os.environ.get('VERDICT_ASSISTANT_ID')
